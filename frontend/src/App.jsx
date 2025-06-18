4f6e80c7932842c27d9e15bcb1a85ba83ca69cc1
didarBro <didar131299@gmail.com>
2025-06-18 12:04:29 +0600

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './pages/home/home'
+// import Home from './pages/home/home'
 import Portfolio from './pages/portfolio/Portfolio'
 function App() {
   const [count, setCount] = useState(0)
